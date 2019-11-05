4846b18e26783877b53f85078c5cd1100f2553d5
unknown <karol.sujeta@outlook.com>
2019-11-05 14:41:01 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
+
+import { FilmListComponent } from './film-list/film-list.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FilmListComponent },
+  { path: 'mainpage', component: TaskListComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
+
+export const routableComponents = [
+  FilmListComponent, TaskListComponent 
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { FilmListComponent } from './film-list/film-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    FilmListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
